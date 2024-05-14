141fded8f1a4f94f87a7e3f0fb18bf99abb4fac2
ariuse1 <ariuse1@gmail.com>
2024-05-14 21:36:08 +0700

    140524

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,53 +9,13 @@ public class Player : MonoBehaviour
     private MoveObject _moveObject;
     private CombatPlayer _combatPlayer;
     private PlayerMover _movePlayer;
-       
+
     private void Awake()
     {
         _animationObject = GetComponent<AnimationObject>();
         _moveObject = GetComponent<MoveObject>();
         _combatPlayer = GetComponent<CombatPlayer>();
         _movePlayer = GetComponent<PlayerMover>();
-    }
-
-    private void FixedUpdate()
-    {
-        Action();
-    }
-     
-    public void Die()
-    {
-    }
-
-    private void Action()
-    {
-        bool isAttack = _combatPlayer.IsWork;
-        StatesAnim stateAnim = StatesAnim.Idle;
-
-
-        if (_movePlayer.isDownHorizontal && !isAttack)
-        {
-            _movePlayer.Run();
-            stateAnim = StatesAnim.Run;
-        }
-            
-        if (_movePlayer.isDownJump && !isAttack)
-        {
-            _movePlayer.Jump();            
-            stateAnim = StatesAnim.Jump;
-        }
-     
-        if (_movePlayer.isDownAttack)
-        {
-            _combatPlayer.Attack();
-
-            if (_combatPlayer.IsAttack)
-            {
-                stateAnim = StatesAnim.Attack;
-            }
-        }
-
-        _animationObject.Run(stateAnim);
     }
 
     private void OnEnable()
@@ -72,5 +32,45 @@ public class Player : MonoBehaviour
         {
             heallth.Died -= Die;
         }
+    }
+
+    private void FixedUpdate()
+    {
+        Action();
+    }
+
+    public void Die()
+    {
+    }
+
+    private void Action()
+    {
+        bool isAttack = _combatPlayer.IsWork;
+        StatesAnim stateAnim = StatesAnim.Idle;
+
+
+        if (_movePlayer.isDownHorizontal && !isAttack)
+        {
+            _movePlayer.Run();
+            stateAnim = StatesAnim.Run;
+        }
+
+        if (_movePlayer.isDownJump && !isAttack)
+        {
+            _movePlayer.Jump();
+            stateAnim = StatesAnim.Jump;
+        }
+
+        if (_movePlayer.isDownAttack)
+        {
+            _combatPlayer.Attack();
+
+            if (_combatPlayer.IsAttack)
+            {
+                stateAnim = StatesAnim.Attack;
+            }
+        }
+
+        _animationObject.Run(stateAnim);
     }
 }
